5e483c328313a12cc08e103e1f88ad9cd09d41a4
Ginger <GrantColwell@users.noreply.github.com>
2019-01-08 20:49:12 -0500

    Made a main method

--- src/main/java/ca/_4976/destinationdeepspace/Robot.java
+++ src/main/java/ca/_4976/destinationdeepspace/Robot.java
@@ -1,9 +1,15 @@
 package ca._4976.destinationdeepspace;
 
 import ca._4976.destinationdeepspace.subsystems.Drive;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Robot extends TimedRobot {
+
+    public static void main(String... args) {
+        RobotBase.startRobot(Robot::new);
+    }
 
     public static OI oi;
     public static Drive drive;
