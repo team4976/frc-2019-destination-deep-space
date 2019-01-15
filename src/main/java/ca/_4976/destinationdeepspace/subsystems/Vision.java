789df22b22fc3bca013f24a40742c57f19dd5fa6
Aavin sidhu <aavinsidhu@hotmail.com>
2019-01-15 16:59:47 -0500

    Added Vision subsystem

---
+++ src/main/java/ca/_4976/destinationdeepspace/subsystems/Vision.java
@@ -0,0 +1,12 @@
+package ca._4976.destinationdeepspace.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Vision extends Subsystem {
+
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+}
