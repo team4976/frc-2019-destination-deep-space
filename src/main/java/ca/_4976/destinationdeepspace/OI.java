87b8bc2976fb4570af67f32b77da857e378fbcd1
Aavin sidhu <aavinsidhu@hotmail.com>
2019-02-07 19:56:16 -0500

    Merged climber onto intake

--- src/main/java/ca/_4976/destinationdeepspace/OI.java
+++ src/main/java/ca/_4976/destinationdeepspace/OI.java
@@ -1,8 +1,10 @@
 package ca._4976.destinationdeepspace;
 
-import ca._4976.destinationdeepspace.commands.shootLeft;
-import ca._4976.destinationdeepspace.commands.shootRight;
+import ca._4976.destinationdeepspace.commands.*;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.command.Command;
 
 // The operator interface of the robot, it has been simplified from the real
 // robot to allow control with a single Xbox joystick. As a result, not all
@@ -11,13 +13,27 @@ import edu.wpi.first.wpilibj.Joystick;
 public final class OI {
 
     public Joystick driver = new Joystick(0);
-
     public Joystick operator = new Joystick(1);
 
     OI() {
-        new JoystickButton(driver, 2).whenPressed(new shootRight());
-        new JoystickButton(driver, 3).whenPressed(new shootLeft());
-        //Dpad sensor for operator controller
 
+        new JoystickButton(driver,4).whileHeld(new intakeFromGround());
+
+        new JoystickButton(operator, 1).whenPressed(new intakeClimb());
+        new JoystickButton(operator, 2).whenPressed(new intakeHPPickup());
+        new JoystickButton(operator, 4).whenPressed(new intakeHome());
+        //Dpad sensor for operator controller
+        if (operator.getPOV() == 0) {
+        } else if (operator.getPOV() == 90) {
+        } else if (operator.getPOV() == 180) {
+        } else if (operator.getPOV() == 270) {
+        }
+
+        //Dpad sensor for driver controller
+        if (driver.getPOV() == 0) {
+        } else if (driver.getPOV() == 90) {
+        } else if (driver.getPOV() == 180) {
+        } else if (driver.getPOV() == 270) {
+        }
     }
 }
--- src/main/java/ca/_4976/destinationdeepspace/subsystems/Intake.java
+++ src/main/java/ca/_4976/destinationdeepspace/subsystems/Intake.java
@@ -1,15 +1,78 @@
 package ca._4976.destinationdeepspace.subsystems;
 
+import ca._4976.destinationdeepspace.Robot;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class Intake extends Subsystem {
+public class Intake extends PIDSubsystem {
 
     NetworkTable intake = NetworkTableInstance.getDefault().getTable("Intake");
 
+    DoubleSolenoid hatchPanelPickUp = new DoubleSolenoid(6,7);
+    boolean HP = true;
+    TalonSRX intakeMover = new TalonSRX(11);
+    TalonSRX intakeMoverSlave = new TalonSRX(12);
+    TalonSRX intakeBall = new TalonSRX(13);
+    AnalogInput cherrySensor = new AnalogInput(2);
+    TalonSRX intakeMotor1 = new TalonSRX(14);
+    TalonSRX intakeMotor2 = new TalonSRX(15);
+    double intakeSetpoint;
+
     @Override
     protected void initDefaultCommand() {
+        hatchPanelPickUp.set(DoubleSolenoid.Value.kForward);
+        intakeMoverSlave.follow(intakeMover);
+    }
+    public void choose(){
+        if(HP) Robot.intake.releaseGear();
+        if(!HP)Robot.intake.holdGear();
+    }
+    public void pickUpBall(){
+        intakeMotor1.set(ControlMode.PercentOutput, 0.5);
+        intakeMotor2.set(ControlMode.PercentOutput, -0.5);
+    }
+    public void end(){
+        intakeMotor1.set(ControlMode.PercentOutput, 0.0);
+        intakeMotor2.set(ControlMode.PercentOutput, 0.0);
+    }
+    public void holdGear(){
+        hatchPanelPickUp.set(DoubleSolenoid.Value.kForward);
+    }
+    public void releaseGear(){
+        hatchPanelPickUp.set(DoubleSolenoid.Value.kReverse);
+    }
+    public void climb(){
 
+    }
+    public void disablePID(){intakeController.disable();}
+    public void setSetpointHome(){intakeSetpoint = 172; intakeController.enable();}
+    public void setSetpointHP(){intakeSetpoint = 83; intakeController.enable();}
+    public void setSetpointClimb(){intakeSetpoint = 17; intakeController.enable();}
+    public void moveIntake(double output){
+        intakeMover.set(ControlMode.PercentOutput, output);
+    }
+    public PIDController intakeController;
+    private final PIDOutput intakeOutput = this::usePIDOutput;
+    public Intake(String name, double p, double i, double d){
+        super(name, p, i, d);
+        intakeController = new PIDController(p, i, d, cherrySensor, intakeOutput);
+        intakeController.setSetpoint(intakeSetpoint);
+    }
+    @Override
+    protected double returnPIDInput() {
+        return cherrySensor.getVoltage()*36;
+    }
+
+    @Override
+    protected void usePIDOutput(double output) {
+        moveIntake(output);
     }
 }
