e7dc9499ac08062fde4dcbbb0278514e937f5b5d
Koi-Boi <aavinsidhu@hotmail.com>
2019-03-19 18:06:44 -0400

    auto_1_18_HP_20

---
+++ src/main/java/ca/_4976/destinationdeepspace/commands/autos/auto_1_18_HP_20.java
@@ -0,0 +1,26 @@
+package ca._4976.destinationdeepspace.commands.autos;
+
+import ca._4976.destinationdeepspace.commands.HPRelease;
+import ca._4976.destinationdeepspace.commands.autoModules.*;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class auto_1_18_HP_20 extends CommandGroup {
+
+    public auto_1_18_HP_20(){
+        addSequential(new DriveForwardsFromOneToRocket());
+        addSequential(new TurnFromRocketToEighteen());
+        addSequential(new DriveForwardsFromRocketToEighteen());
+        addSequential(new Delay());
+        addSequential(new HorizontalCenter());
+        addSequential(new Delay());
+        addSequential(new TurnNinteyDegreesLeft());
+        addSequential(new Delay());
+        addSequential(new DriveForHatch());
+        addSequential(new Delay());
+        addSequential(new HPRelease());
+        addSequential(new Delay());
+        addSequential(new DriveBackwardsABit());
+        addSequential(new TurnFromRocketToEighteen());
+        addSequential(new DriveForwardsFromEighteenToTwenty());
+    }
+}
