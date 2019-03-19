49a39ff9f82522c9afe1765cae11d3ea8d2a0b81
Koi-Boi <aavinsidhu@hotmail.com>
2019-03-19 17:52:02 -0400

    auto_4_6_HP

---
+++ src/main/java/ca/_4976/destinationdeepspace/commands/autos/auto_4_6_HP.java
@@ -0,0 +1,22 @@
+package ca._4976.destinationdeepspace.commands.autos;
+
+import ca._4976.destinationdeepspace.commands.HPRelease;
+import ca._4976.destinationdeepspace.commands.autoModules.*;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class auto_4_6_HP extends CommandGroup {
+
+    public auto_4_6_HP(){
+        addSequential(new DriveForwardsFromGround());
+        addSequential(new TurnNinteyDegreesLeft());
+        addSequential(new DriveForwardsABit());
+        addSequential(new Delay());
+        addSequential(new HorizontalCenter());
+        addSequential(new Delay());
+        addSequential(new TurnNinteyDegreesRight());
+        addSequential(new Delay());
+        addSequential(new DriveForHatch());
+        addSequential(new Delay());
+        addSequential(new HPRelease());
+    }
+}
